c7c77432d6000cc9881286e3c5231ef4b077d348
Faiza <faizaqamar306@gmail.com>
2020-06-15 23:19:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,222 @@
+ //Chapter # 
+ 
+ //Task # 1
+
+ //alert("Error! Pleasw enter a valid password");
+
+//Task # 2
+ 
+ //alert("Welcome to js land ...  \n Happy Coding");
+
+ //Task # 3
+
+//alert("Welcome to js land...");
+
+//Task # 4
+
+//alert("Happy Coding! \n Prevent this page from additional dialogs");
+
+//Task# 5
+
+//alert("Hello... I can run Js through my web browswer's cinsole");
+
+   
+
+
+
+//chapter# 2
+
+//Task # 1
+
+//var name = "Jhone Doe";
+//alert(name);
+
+//Task # 2
+
+//var age = "15 years old";
+//alert(age);
+
+//Task # 3
+
+//var skill = "Certified mobile application development";
+//alert(skill);
+
+//Task # 4
+
+//var name = "PIZZA";
+//window.alert("PIZZA \n PIZZ \n PIZ  \n PI  \n P");
+
+
+//Task # 5
+
+//var email = "Your email address";
+//alert("My email address is example@example.com");
+
+//Task # 6
+
+//var book = "“A smarter way to learn JavaScript”.";
+//alert("I am trying to learn from the Book A smarter way to learn JavaScript");
+
+//Task # 7
+
+//document.write("Yah! I can write HTML content through JAVAScript")
+
+//Task # 8
+
+//alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬");
+
+
+
+
+//Chapter # 3
+
+//Task # 1
+
+//var age = "15 Years old";
+//alert(age);
+
+//Task # 2
+
+//var visitors = "You have visited this  Store 14 times";
+//alert(visitors);
+
+//Task # 3
+
+//document.write("My birth Year is 1990")
+//document.write("Data type of my declared variable is number");
+
+//Task # 4
+
+
+//var VisitorName = prompt("enter your name")
+//var ProductName = prompt("enter product name")
+//var Quantity = prompt("enter quantity")
+//document.write(VisitorName + "ordered" + Quantity + ProductName);
+
+//Chapte # 4
+//Task # 1
+
+//document.write("Rules for naming JS variables")
+
+
+
+
+//Chapter # 5
+//Task # 1
+
+//var add =  "sum of 3 and 5 is 8"
+//document.write("sum of 3 and 5 is 8");
+
+//Task # 2
+
+//document.write("sub of 3 and 5 is 2")
+
+
+
+//document.write("mul of 3 and 5 is 15")
+
+
+
+//document.write("div of 3 and 5 is ")
+
+//Task # 3
+//var number;
+
+//document.write("Value after variable declaration is" + "  " + number + "<br>")
+
+//number = 5;
+
+//document.write("Initial value:" + "  " + number + "<br>")
+
+//number++
+
+//document.write("Value after Increment is:" + "  " + number + "<br>")
+
+//number = number + 7
+
+//document.write("Value after addition is:" + "  " + number + "<br>")
+
+//number-- 
+
+//document.write("Value after decrement is:" + "  " + number + "<br>")
+
+//number = number % 3
+
+//document.write("The remainder is: " + " 0 " + number + "<br>")
+
+//Task # 5
+
+
+//for(var i = 1; i <= 10; i++){
+    //document.write("2" + "x" + i + "=" + 2*i + "<br>")
+ //}
+
+
+
+ 
+//Task # 7
+var p1 = 650;
+var p2 = 3;
+var q1 = 100;
+var q2 = 7;
+var shp = 100;
+var total=p1*p2+q1*q2;
+
+document.write("<br><br> Price of item 1 is:" + "  " + p1 + "<br> Price of item 1 is:" + "   " + p2 + 
+"<br> Ordered quantity of item 2 is:" + "   " + q1 + "<br>  Ordered quantity of item 2 is:" + "  " + q2 
++ "<br>Shipping charges:" + "   " + shp + "<br> Total cost of your order is:" + "   " + total)
+
+//Task # 8
+document.write("<h1>Mark Sheet</h1>")
+
+var totalMarks = 980;
+var marksObtained = 804;
+var per = (marksObtained * 100) / totalMarks
+
+document.write("<br><br> Total marks:" + "  " + totalMarks + "<br> Marks Obtained:" + "  " + marksObtained 
++ "<br> Percentage:" + "  " + per + "%")
+
+
+//Task # 9
+document.write("<h1>Currency in For</h1>")
+
+var dollar = 10;
+var riyal = 25;
+
+var rupees = (10 * 104.80) + (25 * 28)
+document.write("<br><br> Total Currency in PKR:" + "  " + rupees)
+
+
+//Task # 11
+document.write("<h1>Age Calculator</h1>")
+
+var CurrentYear = 2016;
+var BirthYear = 1992;
+var YourAgeis= (CurrentYear - BirthYear)
+
+document.write("<br<br> Current year:" + "   " + CurrentYear + "<br> Birth year:" + "  " + BirthYear 
++ "<br> Your Age is:" + "   " + YourAgeis) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
